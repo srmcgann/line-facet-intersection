50071a190b59913269363eae922f0c64a4de762b
Scott R McGann <s.r.mcgann@hotmail.com>
2023-06-09 10:22:45 -0700

    Update lineFaceI.js

--- lineFaceI.js
+++ lineFaceI.js
@@ -1,8 +1,11 @@
     lineFaceI = (X1, Y1, Z1, X2, Y2, Z2, facet, autoFlipNormals=false, showNormals=false) => {
+      let X_, Y_, Z_, d, m, l_,K,J,L,p
       let I_=(A,B,M,D,E,F,G,H)=>(K=((G-E)*(B-F)-(H-F)*(A-E))/(J=(H-F)*(M-A)-(G-E)*(D-B)))>=0&&K<=1&&(L=((M-A)*(B-F)-(D-B)*(A-E))/J)>=0&&L<=1?[A+K*(M-A),B+K*(D-B)]:0
-      let R_ = (Rl,Pt,Yw)=>{
+      let Q_=()=>[c.width/2+X_/Z_*600,c.height/2+Y_/Z_*600]
+      let R_ = (Rl,Pt,Yw,m)=>{
         let M=Math, A=M.atan2, H=M.hypot
-        X=S(p=A(X,Y)+Rl)*(d=H(X,Y)),Y=C(p)*d,X=S(p=A(X,Z)+Yw)*(d=H(X,Z)),Z=C(p)*d,Y=S(p=A(Y,Z)+Pt)*(d=H(Y,Z)),Z=C(p)*d
+        X_=S(p=A(X_,Y_)+Rl)*(d=H(X_,Y_)),Y_=C(p)*d,X_=S(p=A(X_,Z_)+Yw)*(d=H(X_,Z_)),Z_=C(p)*d,Y_=S(p=A(Y_,Z_)+Pt)*(d=H(Y_,Z_)),Z_=C(p)*d
+        if(m){ X_+=oX,Y_+=oY,Z_+=oZ }
       }
       let rotSwitch = m =>{
         switch(m){
@@ -17,7 +20,7 @@
       let b1 = facet[2][0]-facet[1][0], b2 = facet[2][1]-facet[1][1], b3 = facet[2][2]-facet[1][2]
       let c1 = facet[1][0]-facet[0][0], c2 = facet[1][1]-facet[0][1], c3 = facet[1][2]-facet[0][2]
       let crs = [b2*c3-b3*c2,b3*c1-b1*c3,b1*c2-b2*c1]
-      let d = Math.hypot(...crs)+.001
+      d = Math.hypot(...crs)+.001
       let nls = 1 //normal line length
       crs = crs.map(q=>q/d*nls)
       let X1_ = ax, Y1_ = ay, Z1_ = az
@@ -28,44 +31,45 @@
         flip = d2_>d1_?-1:1
       }
       let X2_ = ax + (crs[0]*=flip), Y2_ = ay + (crs[1]*=flip), Z2_ = az + (crs[2]*=flip)
-
       if(showNormals){
         x.beginPath()
-        X = X1_, Y = Y1_, Z = Z1_
-        R(Rl,Pt,Yw,1)
-        if(Z>0) x.lineTo(...Q())
-        X = X2_, Y = Y2_, Z = Z2_
-        R(Rl,Pt,Yw,1)
-        if(Z>0) x.lineTo(...Q())
-        stroke('#f00','',.5,true)
+        X_ = X1_, Y_ = Y1_, Z_ = Z1_
+        R_(Rl,Pt,Yw,1)
+        if(Z_>0) x.lineTo(...Q_())
+        X_ = X2_, Y_ = Y2_, Z_ = Z2_
+        R_(Rl,Pt,Yw,1)
+        if(Z_>0) x.lineTo(...Q_())
+        x.lineWidth = 5
+        x.strokeStyle='#f004'
+        x.stroke()
       }
       
       let p1_ = Math.atan2(X2_-X1_,Z2_-Z1_)
       let p2_ = -(Math.acos((Y2_-Y1_)/(Math.hypot(X2_-X1_,Y2_-Y1_,Z2_-Z1_)+.001))+Math.PI/2)
       let isc = false, iscs = [false,false,false]
-      X = X1, Y = Y1, Z = Z1
+      X_ = X1, Y_ = Y1, Z_ = Z1
       R_(0,-p2_,-p1_)
-      let rx_ = X, ry_ = Y, rz_ = Z
+      let rx_ = X_, ry_ = Y_, rz_ = Z_
       for(let m=3;m--;){
         if(isc === false){
-          X = rx_, Y = ry_, Z = rz_
+          X_ = rx_, Y_ = ry_, Z_ = rz_
           rotSwitch(m)
-          X1_ = X, Y1_ = Y, Z1_ = Z = 5, X = X2, Y = Y2, Z = Z2
+          X1_ = X_, Y1_ = Y_, Z1_ = Z_ = 5, X_ = X2, Y_ = Y2, Z_ = Z2
           R_(0,-p2_,-p1_)
           rotSwitch(m)
-          X2_ = X, Y2_ = Y, Z2_ = Z
+          X2_ = X_, Y2_ = Y_, Z2_ = Z_
           facet.map((q_,j_)=>{
             if(isc === false){
               let l = j_
-              X = facet[l][0], Y = facet[l][1], Z = facet[l][2]
+              X_ = facet[l][0], Y_ = facet[l][1], Z_ = facet[l][2]
               R_(0,-p2_,-p1_)
               rotSwitch(m)
-              let X3_=X, Y3_=Y, Z3_=Z
+              let X3_=X_, Y3_=Y_, Z3_=Z_
               l = (j_+1)%facet.length
-              X = facet[l][0], Y = facet[l][1], Z = facet[l][2]
+              X_ = facet[l][0], Y_ = facet[l][1], Z_ = facet[l][2]
               R_(0,-p2_,-p1_)
               rotSwitch(m)
-              let X4_ = X, Y4_ = Y, Z4_ = Z
+              let X4_ = X_, Y4_ = Y_, Z4_ = Z_
               if(l_=I_(X1_,Y1_,X2_,Y2_,X3_,Y3_,X4_,Y4_)) iscs[m] = l_
             }
           })
@@ -77,29 +81,29 @@
         ax=0, ay=0, az=0
         facet.map((q_, j_)=>{ ax+=q_[0], ay+=q_[1], az+=q_[2] })
         ax/=facet.length, ay/=facet.length, az/=facet.length
-        X = ax, Y = ay, Z = az
+        X_ = ax, Y_ = ay, Z_ = az
         R_(0,-p2_,-p1_)
-        X1_ = X, Y1_ = Y, Z1_ = Z
+        X1_ = X_, Y1_ = Y_, Z1_ = Z_
         X2_ = iscx, Y2_ = iscy, Z2_ = iscz
         facet.map((q_,j_)=>{
           if(pointInPoly){
             let l = j_
-            X = facet[l][0], Y = facet[l][1], Z = facet[l][2]
+            X_ = facet[l][0], Y_ = facet[l][1], Z_ = facet[l][2]
             R_(0,-p2_,-p1_)
-            let X3_ = X, Y3_ = Y, Z3_ = Z
+            let X3_ = X_, Y3_ = Y_, Z3_ = Z_
             l = (j_+1)%facet.length
-            X = facet[l][0], Y = facet[l][1], Z = facet[l][2]
+            X_ = facet[l][0], Y_ = facet[l][1], Z_ = facet[l][2]
             R_(0,-p2_,-p1_)
-            let X4_ = X, Y4_ = Y, Z4_ = Z
+            let X4_ = X_, Y4_ = Y_, Z4_ = Z_
             if(I_(X1_,Y1_,X2_,Y2_,X3_,Y3_,X4_,Y4_)) pointInPoly = false
           }
         })
         if(pointInPoly){
-          X = iscx, Y = iscy, Z = iscz
+          X_ = iscx, Y_ = iscy, Z_ = iscz
           R_(0,p2_,0)
           R_(0,0,p1_)
-          isc = [[X,Y,Z], [crs[0],crs[1],crs[2]]]
+          isc = [[X_,Y_,Z_], [crs[0],crs[1],crs[2]]]
         }
       }
       return isc
-    }    
+    }
